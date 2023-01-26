f958e3405769460261ebf91e03f23098cfcf20b7
BuildTools <unconfigured@null.spigotmc.org>
2023-01-26 17:15:51 +0700

    Fix Layout, Drag & Drop

--- components/footer/footer.js
+++ components/footer/footer.js
@@ -9,9 +9,9 @@ export default class Footers extends React.Component {
   render() {
     return (
       <footer className="bg-gray-900">
-        <div className="max-w-7xl mx-auto py-12 px-4 overflow-hidden sm:px-6 lg:px-8">
+        <div className="max-w-7xl mx-auto py-5 px-4 overflow-hidden sm:px-6 lg:px-8">
           <div className="text-center text-base text-white">
-            &copy; 2023, <Link href='https://www.13xlabs.com/' target='_blank'>13X Labs</Link>. All rights reserved.
+            &copy; 2023 - <Link href='https://www.13xlabs.com/' target='_blank'>13X Labs</Link>. All rights reserved.
           </div>
         </div>
       </footer>
--- src/pages/index.js
+++ src/pages/index.js
@@ -32,9 +32,9 @@ export default class Home extends React.Component {
       precentageUpload: 0,
       value: '',
       copied: false,
+      setDragActive: false
     };
 
-    // this.uploadImage = this.uploadImage.bind(this)
   }
 
   componentDidMount = () => {
@@ -52,9 +52,7 @@ export default class Home extends React.Component {
       const params = {
           Bucket: BucketName,
           Key: e.target.files[0].name, // File name you want to save as in S3
-          Body: file,
-          ACL: 'public-read',
-          ContentType: 'image/jpeg'
+          Body: file
       };
       
       let that = this;
@@ -84,8 +82,82 @@ export default class Home extends React.Component {
       })
     })
 
-    
+    this.setState({
+      setDragActive: false
+    })    
   }
+
+  // handle drag and drop file
+  handleDragEnter = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    this.setState({
+      setDragActive: true
+    })
+  }
+
+  handleDragLeave = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    this.setState({
+      setDragActive: false
+    })
+  }
+
+  handleDragOver = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    this.setState({
+      setDragActive: true
+    })
+  }
+
+  handleDrop = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    this.setState({
+      setDragActive: false
+    })
+    if (e.dataTransfer.files && e.dataTransfer.files[0]) {
+      let file = e.dataTransfer.files[0];
+
+      let formData = new FormData();
+      formData.append("file", file);
+
+      // Setting up S3 upload parameters
+      const params = {
+          Bucket: BucketName,
+          Key: e.dataTransfer.files[0].name, // File name you want to save as in S3
+          Body: file
+      };
+      
+      let that = this;
+      let options = {partSize: 10 * 1024 * 1024, queueSize: 1};
+      // Uploading files to the bucket
+      s3.upload(params, options, function(e, data) {
+        that.setState({
+          kData: data
+        })
+      }).on('httpUploadProgress', function(e) {
+        let percentCompleted = Math.round((e.loaded * 100) / e.total)
+        if (percentCompleted <= 100) {
+          that.setState({
+            precentageUpload: percentCompleted
+          })
+        }
+      });
+    }
+    axios.post(StorjAuth, {
+      access_grant: AccessGrant,
+      public: true,
+    },
+    ).then((res) => {
+      this.setState({
+        access: res.request.response
+      })
+    })
+  }
+
 
   getUploadLink = () => {
     if (this.state.access) {
@@ -101,6 +173,17 @@ export default class Home extends React.Component {
     }
   }
 
+  restartUpload = () => {
+    this.setState({
+      urlImage: null,
+      kData: [],
+      precentageUpload: 0,
+      value: '',
+      copied: false,
+      setDragActive: false
+    })
+  }
+
 
   render() {
     this.getUploadLink()
@@ -108,8 +191,8 @@ export default class Home extends React.Component {
     return (
       <>
         <Head>
-          <title>File Sharing - Upload File to IPFS | 13X Labs</title>
-          <meta name="description" content="File Sharing - Upload File to IPFS by 13XLabs. 13X Labs is a R&D Technology company focusing on blockchain, dApp, Web3, Storage3, etc." />
+          <title>Decentralized Upload & Sharing Platform | 13X Labs</title>
+          <meta name="description" content="Decetralized upload & sharing large files is a simple, easy-to-use tool for sharing files directly from your device without having to rely on third-party intermediaries like big corporate cloud providers." />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <meta property="og:image" content="https://uploadfile.on.btfs.io/uploadfiletoipfs.png" ></meta>
           <meta property="twitter:image" content="https://uploadfile.on.btfs.io/uploadfiletoipfs.png"></meta>
@@ -121,11 +204,14 @@ export default class Home extends React.Component {
             <Navbar />
             {/* Drop File to Attach or Browser */}
             <div className=" h-screen w-screen sm:px-8 md:px-16 sm:py-8 bg-gray-700">
-              <div className="container mx-auto max-w-screen-lg h-full">
+              <div className="container mx-auto max-w-screen-lg h-auto">
                 <article aria-label="File Upload Modal" className="relative h-full flex flex-col bg-white shadow-xl rounded-md" >
-                  <section className="h-full overflow-auto p-8 w-full h-full flex flex-col">
-                    <label
-                        className="flex justify-center w-full h-32 px-4 transition bg-white border-2 border-gray-300 border-dashed rounded-md appearance-none cursor-pointer hover:border-gray-400 focus:outline-none">
+                  <section className="h-full overflow-auto p-8 w-full flex flex-col">
+                    {this.state.precentageUpload ? <div></div> : <label
+                        for="dropzone-file"
+                        className="flex justify-center w-full h-32 px-4 transition bg-white border-2 border-gray-300 border-dashed rounded-md appearance-none cursor-pointer hover:border-gray-400 focus:outline-none"
+                        onDragEnter={this.handleDragEnter} onDragLeave={this.handleDragLeave} onDragOver={this.handleDragOver} onDrop={this.handleDrop}
+                        >
                         <span className="flex items-center space-x-2">
                             <ArrowUpTrayIcon className="block h-6 w-6" aria-hidden="true" />
                             <span className="font-medium text-gray-600">
@@ -133,8 +219,9 @@ export default class Home extends React.Component {
                             <a className="mt-2 rounded-sm px-3 py-1 bg-gray-200 hover:bg-gray-300 focus:shadow-outline focus:outline-none">Upload a file</a>
                             </span>
                         </span>
-                        <input id='file-input' type='file' onChange={this.uploadImage.bind(this)} className="hidden" />
-                    </label>
+                        <input id='dropzone-file' type='file' onChange={this.uploadImage.bind(this)} className="hidden" />
+                    </label>}
+                    
                     <div className="w-full bg-gray-200 rounded-full dark:bg-gray-700">
                     {this.state.precentageUpload == 100 ? 
                                       <div className="bg-gray-600 text-xs font-medium text-white text-center p-1 leading-none rounded-full">Completed</div>
@@ -161,6 +248,9 @@ export default class Home extends React.Component {
                       <p className='text-center mt-2'>QR Code Generator</p>
                       <div className='mt-1 flex justify-center'>
                         <QRCode value={`${this.state.urlImage}`} size={128} />
+                      </div>
+                      <div className='block text-center'>
+                        <button onClick={this.restartUpload} className='bg-gray-600 text-xs font-medium text-white text-center p-2 mt-3 leading-none rounded-full'>Upload Again</button>
                       </div>
                     </div>
                     : null}
